67d2d37e752fe147bd25ce10e97f7b1e60c164e8
clajulietta <clarissajulieta07@gmail.com>
2026-09-23 02:17:10 +0700

    menghubungkan halaman search dan kategori

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/material.dart';
+
 import 'search/search_screen.dart';
 import 'category/category_screen.dart';
-import 'package:flutter/material.dart';
+
 import 'widgets/search_bar_widget.dart';
 import 'widgets/promo_banner.dart';
 import 'widgets/category_list.dart';
 import 'widgets/product_grid.dart';
-import 'data/dummy_products.dart';
 
+import 'data/dummy_products.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,63 +20,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Marketplace'),
-          backgroundColor: Colors.green,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SearchScreen(),
-                  ),
-                );
-              },
+      debugShowCheckedModeBanner: false,
+
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Marketplace'),
+              backgroundColor: Colors.green,
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SearchScreen(),
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.category),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CategoryScreen(),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-            IconButton(
-              icon: const Icon(Icons.category),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const CategoryScreen(),
+
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SearchBarWidget(),
+
+                  const SizedBox(height: 16),
+
+                  const PromoBanner(),
+
+                  const SizedBox(height: 16),
+
+                  const Text(
+                    'Kategori',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
                   ),
-                );
-              },
+
+                  const SizedBox(height: 8),
+
+                  const CategoryList(),
+
+                  const SizedBox(height: 16),
+
+                  const Text(
+                    'Rekomendasi Untukmu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  ProductGrid(
+                    products: dummyProducts,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SearchBarWidget(),
-              const SizedBox(height: 16),
-              const PromoBanner(),
-              const SizedBox(height: 16),
-              const Text(
-                'Kategori',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize:16),
-              ),
-              const SizedBox(height: 8),
-              const CategoryList(),
-              const SizedBox(height: 16),
-              const Text(
-                'Rekomendasi Untukmu',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              const SizedBox(height: 8),
-              ProductGrid(products: dummyProducts),
-            ],
-          ),
-        ),
+          );
+        },
       ),
     );
   }
 }
-
